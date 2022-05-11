a7ebb442b16d738aa7eb7d9b31cb60e70c2e6429
Sanchit Tewari <sanchittewari222@gmail.com>
2022-05-11 13:26:05 +0530

    .env

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
+let messageStyle = process.env.MESSAGE_STYLE;
+
+app.get("/json", (request,response) => {
+    const message = messageStyle==="uppercase"?"HELLO JSON":"Hello json";
+    response.json({"message":message});
+    console.log(messageStyle);
+})
+
+
 
 
 
